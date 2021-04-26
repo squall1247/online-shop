f9498bd2cd9f76d5b3b9e08fb5146812b9db7fbc
Clyde <squall1247@gmail.com>
2021-04-26 17:06:25 +0800

    Enhance error handling

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,7 +12,9 @@ exports.getProducts = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
     });
 };
 
@@ -27,7 +29,11 @@ exports.getProduct = (req, res, next) => {
         isAuthenticated: req.user
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+    });
 }
 
 exports.getIndex = (req, res, next) => {
@@ -40,7 +46,9 @@ exports.getIndex = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
     });
 };
 
@@ -79,7 +87,11 @@ exports.postCartDeleteProduct =(req, res, next) => {
     .then(result => {
       res.redirect('/cart');
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+    });
 };
 
 exports.postOrder = (req, res, next) => {
@@ -105,7 +117,11 @@ exports.postOrder = (req, res, next) => {
     .then(result => {
       res.redirect('/orders');
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+    });
 };
 
 exports.getOrders = (req, res, next) => {
@@ -118,7 +134,11 @@ exports.getOrders = (req, res, next) => {
         isAuthenticated: req.user
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+    });
 };
 
 exports.getCheckout = (req, res, next) => {
