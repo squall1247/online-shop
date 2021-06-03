afec12a6efd64128aa9ff5fa1c2097a3fd0b8442
Clyde <squall1247@gmail.com>
2021-06-04 00:35:44 +0800

    Use environment variables

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,8 +5,6 @@ const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
-const Setting = require('../util/dbsetting');
-// const Skey = Setting.stripeKey;
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const ITEMS_PER_PAGE = 2;
